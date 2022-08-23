4b65f944bf5d31ed673f775257488be079b3a40a
Jenos <sone1118@naver.com>
2022-08-23 19:37:00 +0900

    hello.js 사용자 정의 함수 추가 jsx설명 추가

--- react_app2/src/App.js
+++ react_app2/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import Hello from "./hello";
 import './App.css';
 
 function App() {
+  const name = 'react';
+  //스타일을 넣어줄때에는 객체로 넣어줘야한다
+  //background-color => backgroundColor와 같이 변환해서 넣어준다
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello></Hello>
+      {/*jsx에서 js를 사용할때는 {} 를 사용한다
+      class 를 사용하고 싶으면 className이라는 것을 사용한다 */}
+      <div style={style}>{name}</div>
+      <div className='gray-box'></div>
+      <Hello></Hello>
     </div>
   );
 }
