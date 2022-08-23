a9cd7705df257a3e6c9bdd72c6db8569ddf41203
Jenos <sone1118@naver.com>
2022-08-23 22:24:31 +0900

    Input이 여러개일때 객체, 배열의 불변성을 위해서 복제후 값을 변경 해야한다. (이전값과, 현재값이 달라아해)

--- react_app2/src/InputSample.js
+++ react_app2/src/InputSample.js
@@ -1,20 +1,51 @@
 import React, {useState} from "react";
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+    const { name, nickname } = inputs;
+
     const onChange = (e) => {
-        setText(e.target.value);
+        //꼭 객체값을 복사한후에 사용해야합니다
+        //객체의 불변성을 지킨다.
+        //inputs[name]을 직접 바꾼다면 아무것도 일어나지않는다.
+        const { name, value } = e.target;
+        /*
+        const nextInputs = {
+            ...inputs,
+            [name]: value
+        }
+        //nextInputs[name] = value;
+        setInputs(nextInputs);
+        */
+       //불변성을 위해서 객체를 복제해서 값을 넣어주는 것임
+       setInputs({
+        ...inputs,
+        [name]: value
+       });
     }
     const deleteAll = () => {
-        setText('');
+
+        setInputs({name: '', nickname: ''});
     }
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input 
+            name='name' 
+            onChange={onChange} 
+            value={name}
+            />
+            <input 
+            name='nickname' 
+            onChange={onChange} 
+            value={nickname}
+            />
             <button onClick={deleteAll}>초기화</button>
             <div>
-                <b>값: </b>
-                {text} 
+                <b>값</b>
+                {name}{nickname}
             </div>
         </div>
     );
