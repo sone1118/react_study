e2dc9afc5ceced6d108f73b8f2e7cc08efdefb08
Jenos <sone1118@naver.com>
2022-08-23 21:26:14 +0900

    조건부 렌더링 isSpecial 값에 따라 조건부 렌더링

--- react_app2/src/App.js
+++ react_app2/src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <Wrapper>
     <div className="App">
-      <Hello name='react' color='red'></Hello>
+      {/* isSpecial 값에 아무것도 넣어주지 않으면 true입니당*/}
+      <Hello name='react' color='red' isSpecial={true}></Hello>
       <Hello color='aqua'></Hello>
     </div>
     </Wrapper>
--- react_app2/src/hello.js
+++ react_app2/src/hello.js
@@ -3,10 +3,20 @@ import React from 'react';
 //비구조 할당 가능
 //객체를 객체내부 이름이랑 똑같은 거로 가져오니까
 //function Hello(props) 말고
-function Hello({color, name}) {
-    return <div style={{
+
+//조건부 렌더링 isSpecial
+//내용이 달라져야 할때 많이 사용한다.
+function Hello({color, name, isSpecial}) {
+    return (<div style={{
         color
-    }}>안녕하세요 {name}</div>
+        }}>
+            {isSpecial ? <b>스페셜하다</b> : <b>스페셜하지않다.</b>}
+            {/*보통 내용이 아애 달라져야 할때는 삼항연산자
+            내용을 추가할때 는 &&를 많이 쓴다고 합니다*/}
+            {isSpecial && <b>*</b>}
+            안녕하세요 {name}
+        </div>
+    );
 }
 
 //props의 default값을 설정해 둘수 있다
