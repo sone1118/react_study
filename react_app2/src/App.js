6babfbf4c5084a7efd6ea4fc29fe347246a65238
Jenos <sone1118@naver.com>
2022-08-24 18:59:25 +0900

    Hook {useEffect, useMemo, useCallback} 설명 추가

--- react_app2/src/App.js
+++ react_app2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 //import Hello from "./hello";
 import Wrapper from "./wrapper"
 //import Counter from "./Counter";
@@ -51,7 +51,7 @@ function App(props) {
 //초기값을 4로 설정
 const nextId = useRef(4);
 
-const onCreate = () => {
+const onCreate = useCallback(() => {
   //console.log(nextId.current);
   nextId.current += 1;
 
@@ -75,7 +75,7 @@ const onCreate = () => {
     username: '',
     email: '',
   });
-}
+}, [username, email, users]);
 
 // const onRemove = (e) => {
 //   const id = Number(e.target.value);
@@ -83,17 +83,29 @@ const onCreate = () => {
 //   console.log(users);
 // }
 
-const onRemove = id => {
+const onRemove = useCallback( id => {
   setUsers(users.filter(user => user.id !== id));
-}
+}, [users]);
 
-const onToggle = id => {
+const onToggle = useCallback( id => {
   //불변성을 위해서 복제후에 넣어줘야해!
   setUsers(users.map(user => 
     user.id === id ? 
     {...user, active: !user.active} : {...user}
   ))
+}, [users]);
+
+//성능 최적을 위해서 useMemo 를 사용한다
+//createUser에 onchage 이벤트를 발생시킬때마다
+//컴포넌트는 rerendering됨 따라서 App 에 있는 CountActiveUser도 계속 실행되는 것
+// 특정 값이 바뀌었을때만 함수를 실행해서 최적화를 하는 것.
+// 즉 users 값이 바뀌었을때 함수가 실행된다.
+// useMeno(() => {}, [users])
+const countActiveUsers = (users) => {
+  console.log('활성 사용자수 세는 중');
+  return users.filter(user => user.active).length;
 }
+const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <Wrapper>
@@ -109,6 +121,7 @@ const onToggle = id => {
       onRemove={onRemove} 
       onToggle={onToggle}
       />
+      <div>활성 사용자 수: {count}</div>
     </div>
     </Wrapper>
   );
--- react_app2/src/UserList.js
+++ react_app2/src/UserList.js
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useEffect } from "react";
+//Hook 함수 (useEffect, useMemo, useCallback)
+
+/*
+useEffect Hook
+처음 화면에 나타날때, 화면에서 사라지게 될때
+컴포넌트의 props나 상태가 update될때, 되기 전에
+컴포넌트가 mount 또는 unmount될때 (나타날때, 삭제될때)
+rerendering될때마다
+특정한 것을 실행 시킴
+
+실제로 사용할때는
+보통 웹에서 페이지를 이동? 할때
+url 맨뒤에 이름이 바뀌잖아. http~~~/clip 같이
+이 정보가 바뀌면 Post를 불러온다 같이 사용할때 쓴다고함
+    
+useEffect(() => {
+    loadPost(username, urlSlug);
+}, [username, urlSlug]);
+
+뭐 요런 너낌?
+*/
+
+/*
+useMemo
+성능을 최적화 할때 사용
+특정한 값이 바뀌었을때만 함수를 실행한다. App.js에 예제 있음
+*/
+
+/*
+useCallback
+함수를 새로 만들지 않고 재사용할때
+특정한 값이 바꼈을 경우에만 새로 만들어서 사용
+그렇지 않을 경우에는 기존의 함수를 재사용
+[]에 그 함수 안에서 사용한 props, state 들이 있으면 꼭 배열에 넣어줘야한다
+안넣어주면 최신상태를 보고 확인 하는 것이 아니라 예전 상태를 보고
+참조하게 되기때문에 의도지않은 상황을 발생 시킬 수 있따.
+useMemo와 비슷 App.js에 예제 있음
+ */
+
 
 function User({user, onRemove, onToggle}) {
     const { id, username, email, active} = user;
+
+    /*
+    //useEffect(()=>{},[]) []은 depencylist인데
+    //이 값이 change될때만 일어난다.
+    //[] 비어있으면
+    // 컴포넌트가 처음 실행될때, 컴포넌트가 사라지기 직전에 호출
+
+    useEffect(() => {
+        console.log('컴포넌트가 화면에 나타난다.');
+        //props값을 state로 넣어줄때
+        //REST API
+        //D3 Video.js 같은것을 준비
+        //setInterval, setTimeout
+        //이미 DOM 이만들어진 상태라 DOM관련한 것도 해도됨
+
+        return () => {
+            console.log('컴포넌트가 화면에서 사라짐');
+        };
+        //뒷정리 함수
+        //업데이트 바로 직전에 호출
+        //컴포넌트가 사라질때 호출된다
+        //clearInterval, clearTimeout
+        // 라이브러리 인스턴스 제거 (video 같은거)
+    }, []);
+*/
+    //useEffect 내부에서 사용할 props 값이나 state 값이 있다면
+    // 2번째 인자 배열로 넣어주는 것이 
+    // 최신의 값을 가르키게 하는 것이라 꼭 넣어주도록 하자.
+    // 2번째 값이 들어 있다면
+    // 컴포넌트가 생성될때, 업데이트 되면 실행
+    // 컴포넌트가 사라질때, 업데이트 되기 직전에 실행
+
+/*
+    useEffect(() => {
+        console.log('user 값이 설정된다');
+        console.log(user);
+
+        //업데이트 바로 직전에 호출 뒷정리함수
+        return () => {
+            console.log('user값이 바뀌기 전');
+            console.log(user);
+        }
+    }, [user]);
+*/
+
     return(
         <div>
             <b style={{
