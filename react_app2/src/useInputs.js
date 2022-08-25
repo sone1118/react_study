c650651442ebbaab749c81a1eaaa5364f637d804
Jenos <sone1118@naver.com>
2022-08-25 22:09:10 +0900

    커스텀 Hook 만들기 useReducer로 수정

--- react_app2/src/useInputs.js
+++ react_app2/src/useInputs.js
@@ -1,17 +1,49 @@
-import  {useState, useCallback } from 'react';
+import  {useReducer, useCallback } from 'react';
 
 //커스텀 hook 만들기
 //use라고 시작하게 만들기
+function reducer(state, action) {
+    switch(action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                [action.name]: action.value
+            };
+        case 'RESET':
+            return Object.keys(state).reduce((acc, cur) => {
+                acc[cur] = '';
+                return acc;
+            }, {});
 
+        default:
+            return state;
+    }
+}
 function useInputs(initialForm) {
-    const [form, setForm] = useState(initialForm);
+    // const [form, setForm] = useState(initialForm);
 
+    // const onChange = useCallback(e => {
+    //     const { name, value } = e.target;
+    //     setForm(form => ({...form, [name]: value}));
+    // }, []);
+    
+    // const reset = useCallback(() => {setForm(initialForm)}, [initialForm]);
+
+    const [form, dispatch] = useReducer(reducer, initialForm);
     const onChange = useCallback(e => {
         const { name, value } = e.target;
-        setForm(form => ({...form, [name]: value}));
+        dispatch({
+            type: 'CHANGE',
+            name,
+            value
+        });
     }, []);
-    
-    const reset = useCallback(() => {setForm(initialForm)}, [initialForm]);
+
+    const reset = useCallback(() => {
+        dispatch({
+            type: 'RESET'
+        });
+    }, [])
 
     return [form, onChange, reset];
 }
