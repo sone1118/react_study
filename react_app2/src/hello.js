7f38c2e8bf6ae39ba4c3d1994b9c6dbac9af1d5e
Jenos <sone1118@naver.com>
2022-08-26 19:27:41 +0900

    class형 Component 기본 설명

--- react_app2/src/hello.js
+++ react_app2/src/hello.js
@@ -1,27 +1,50 @@
+import { Component } from "react";
 import React from 'react';
 
 //비구조 할당 가능
 //객체를 객체내부 이름이랑 똑같은 거로 가져오니까
 //function Hello(props) 말고
 
-//조건부 렌더링 isSpecial
-//내용이 달라져야 할때 많이 사용한다.
-function Hello({color, name, isSpecial}) {
-    return (<div style={{
-        color
-        }}>
-            {isSpecial ? <b>스페셜하다</b> : <b>스페셜하지않다.</b>}
-            {/*보통 내용이 아애 달라져야 할때는 삼항연산자
-            내용을 추가할때 는 &&를 많이 쓴다고 합니다*/}
-            {isSpecial && <b>*</b>}
-            안녕하세요 {name}
-        </div>
-    );
+
+//class형으로 component 만들기
+class Hello extends Component{
+    static defaultProps = {
+        name: '이름없음'
+    };
+
+    render() {
+        const { color, isSpecial, name } = this.props;
+        return (
+            <div style={{color}}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        );
+    }
 }
 
-//props의 default값을 설정해 둘수 있다
-Hello.defaultProps = {
-    name: '이름없음'
-}
+
+
+
+
+//조건부 렌더링 isSpecial
+//내용이 달라져야 할때 많이 사용한다.
+// function Hello({color, name, isSpecial}) {
+//     return (<div style={{
+//         color
+//         }}>
+//             {isSpecial ? <b>스페셜하다</b> : <b>스페셜하지않다.</b>}
+//             {/*보통 내용이 아애 달라져야 할때는 삼항연산자
+//             내용을 추가할때 는 &&를 많이 쓴다고 합니다*/}
+//             {isSpecial && <b>*</b>}
+//             안녕하세요 {name}
+//         </div>
+//     );
+// }
+
+// //props의 default값을 설정해 둘수 있다
+// Hello.defaultProps = {
+//     name: '이름없음'
+// }
 
 export default Hello;
--- react_app2/src/index.js
+++ react_app2/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Hello from './hello'
 import reportWebVitals from './reportWebVitals';
 //import ContextSample from './ContextSample';
 
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
   <div>
-    <App />
+    <Hello />
   </div>
   //</React.StrictMode>
 );
