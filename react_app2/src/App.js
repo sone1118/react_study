1abff1c11a5a036473916b3cc82e18e8f6a9ffc0
Jenos <sone1118@naver.com>
2022-08-29 20:47:47 +0900

    styled-components lib install styled-components 를 사용해서 circle 만들기 템플리터럴`${}`, 태그탬플리터럴 사용 css`${}`

--- react_app2/src/App.js
+++ react_app2/src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
-import CheckBox from './components/CheckBox';
-import './App.scss';
+import React from 'react';
+import styled, { css } from 'styled-components';
 
-function App(props) {
-  const [check, setCheck] = useState(false);
-  const onChange = (e) => {
-    setCheck(e.target.checked);
-  };
+//div tag에다 특정 스타일을 적용한다
+//템플리터럴은 안에 ${} 쓸수가 없어 그냥 템플리터럴이지
+//태그 템프리터럴이 아니기 때문 => css를 추가해줘야한다.
+//=css`블라블라`
+//import styled, { css } from 'styled-components';
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props) => props.color};
+  border-radius: 50%;
+  ${(props) =>
+    props.huge &&
+    css`
+      width: ${props.size};
+      height: ${props.size};
+    `}
+`;
+
+function App() {
   return (
-    <div className="App">
-      <CheckBox onClick={onChange} checked={check}>
-        약관에 모두 동의
-      </CheckBox>
+    <div>
+      <Circle color="pink" huge size="20rem" />
+      <Circle color="green" />
     </div>
   );
 }
